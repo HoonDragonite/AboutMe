706d6928bc030f6b51606d28a750a7676c89f7ce
HoonDragonite <lsoonnoon@gmail.com>
2020-10-15 17:43:33 +0900

    Post JPA Test

--- src/main/java/com/hoondragonite/aboutme/controller/WebRestController.java
+++ src/main/java/com/hoondragonite/aboutme/controller/WebRestController.java
@@ -1,23 +1,24 @@
 package com.hoondragonite.aboutme.controller;
 
+import com.hoondragonite.aboutme.domain.posts.PostsRepository;
+import com.hoondragonite.aboutme.domain.posts.PostsSaveRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/test")
     public String test(){
         return "Hello World";
     }
 
-    @RequestMapping(value = "/back", method = RequestMethod.GET)
-    public String test(HttpServletRequest request){
-        String referer = request.getHeader("Referer");
-        return "redirect:"+ referer;
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity()); // 실제로는 이런 형태 : this.postsRepository = postsRepository;
     }
 }
--- study.md
+++ study.md
@@ -21,6 +21,36 @@ Example.builder()
     .build();
 어느 필드에 어떤 값을 채워야 할지 명확하게 인지할 수 있습니다.
 
+Controller에서 @RequestBody로 외부에서 데이터를 받는 경우엔 기본생성자 + set메소드를 통해서만 값이 할당됩니다.
+그래서 이때만 setter를 허용합니다.
 
 ibatis/MyBatis 등에서 Dao라고 불리는 DB Layer 접근자입니다.
 JPA에선 Repository라고 부르며 인터페이스로 생성합니다.
+
+<br>
+
+## 테스팅
+
+- given : 테스트 기반 환경을 구축하는 단계, 여기선 @builder의 사용법도 같이 확인
+- when : 테스트 하고자 하는 행위 선언, 여기선 Posts가 DB에 insert 되는것을 확인하기 위함
+- then : 테스트 결과 검증, 실제로 DB에 insert 되었는지 확인하기 위해 조회후, 입력된 값 확인
+
+DB가 설치가 안되어있는데 Repository를 사용할 수 있는 이유는, SpringBoot에서의 테스트 코드는 메모리 DB인 H2를 기본적으로 사용하기 때문입니다.
+테스트 코드를 실행하는 시점에 H2 DB를 실행시킵니다.
+테스트가 끝나면 H2 DB도 같이 종료됩니다.
+
+## Bean
+
+스프링프레임워크에선 Bean 을 주입받는 방식들이 아래와 같이 있는데요.
+
+1. @Autowired
+2. setter
+3. 생성자  
+
+이중 가장 권장하는 방식이 생성자로 주입받는 방식입니다. (@Autowired는 비권장방식입니다.)  
+즉, 생성자로 Bean 객체를 받도록 하면 @Autowired와 동일한 효과를 볼 수 있다는 것입니다.
+
+## Entity Class
+
+실제 DB와 매칭될 Class 이다.  
+절대로 테이블과 매핑되는 Entity 클래스를 Request/ Response 클래스로 사용해서는 안됩니다.
