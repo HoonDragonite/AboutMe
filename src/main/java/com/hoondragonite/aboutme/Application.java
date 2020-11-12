ac61f33e572107bd25e35f200cdb481bff9aff99
HoonDragonite <lsoonnoon@gmail.com>
2020-11-12 17:33:52 +0900

    google login error

--- src/main/java/com/hoondragonite/aboutme/Application.java
+++ src/main/java/com/hoondragonite/aboutme/Application.java
@@ -3,7 +3,9 @@ package com.hoondragonite.aboutme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
 public class Application {
 
