65da226276c810e50e156145994309ca61304821
HoonDragonite <lsoonnoon@gmail.com>
2020-11-17 10:53:22 +0900

    Send Data To Controller By POST Method

--- src/main/java/com/hoondragonite/aboutme/controller/UserinfoController.java
+++ src/main/java/com/hoondragonite/aboutme/controller/UserinfoController.java
@@ -5,13 +5,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Controller
 public class UserinfoController {
-
+    //GET,POST 모두 수행
     @RequestMapping(value="/userinfo")
-    public String userInfo(Model model, HttpSession httpSession){
+    public String userInfo(Model model, HttpSession httpSession, HttpServletRequest httpServletRequest){
         SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
@@ -22,6 +23,13 @@ public class UserinfoController {
             model.addAttribute("userName", user.getName());
         }
 
+        System.out.println("My UserInfo *****************");
+        System.out.println("korName : " + httpServletRequest.getParameter("korName"));
+        System.out.println("engName : " + httpServletRequest.getParameter("engName"));
+        System.out.println("email : " + httpServletRequest.getParameter("email"));
+        System.out.println("contact : " + httpServletRequest.getParameter("contact"));
+        System.out.println("blog : " + httpServletRequest.getParameter("blog"));
+        System.out.println("selfIntroduce : " + httpServletRequest.getParameter("selfIntroduce"));
         return "userinfo";
     }
 }
