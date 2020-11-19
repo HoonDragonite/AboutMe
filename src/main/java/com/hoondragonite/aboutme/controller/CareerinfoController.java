c95374d44294b6d8e4f474927dc34e96b18f6825
Bae <qpyu66@gmail.com>
2020-11-19 21:42:25 +0900

    career

--- src/main/java/com/hoondragonite/aboutme/controller/CareerinfoController.java
+++ src/main/java/com/hoondragonite/aboutme/controller/CareerinfoController.java
@@ -2,7 +2,11 @@ package com.hoondragonite.aboutme.controller;
 
 import com.hoondragonite.aboutme.config.auth.dto.CareerinfoDto;
 import com.hoondragonite.aboutme.config.auth.service.CareerinfoService;
+import com.hoondragonite.aboutme.domain.careerinfo.Careerinfo;
+import com.hoondragonite.aboutme.repository.CareerinfoRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.SpringApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,30 +14,34 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class CareerinfoController {
 
     private CareerinfoService careerinfoService;
+    private CareerinfoRepository careerinfoRepository;
 
     public CareerinfoController(CareerinfoService careerinfoService){
         this.careerinfoService = careerinfoService;
     }
 
     @RequestMapping(value = "/cinfo")
-    public String clist(){
-        return "careerinfo.html";
+    public String clist(Model model, CareerinfoDto careerinfoDto){
+        List<CareerinfoDto> cdtolist = careerinfoService.getCareerList();
+        //careerinfoDto.getCno();
+        model.addAttribute("clist",cdtolist);
+        return "careerinfo";
     }
 
-
-//    @RequestMapping(value = "/savecareer")
-//    public String savecareer(CareerinfoDto careerinfoDto, RedirectAttributes redirectAttributes){
-//        careerinfoService.saveCareer(careerinfoDto);
-//        //redirectAttributes.addFlashAttribute("careerinfoDto",careerinfoDto);
-//        return "redirect:/cinfo";
-//    }
-
+    @RequestMapping("/savecareer")
+    //@ResponseBody
+    public void writeCareer(CareerinfoDto careerinfoDto) throws Exception{
+        System.out.printf("careerinfoDto > "+careerinfoDto);
+        careerinfoService.saveCareer(careerinfoDto);
+        //return "careerinfo";
+    }
 
 //    @RequestMapping(value = "/savecareer")
 //    @ResponseBody
@@ -49,30 +57,8 @@ public class CareerinfoController {
 //    }
 
 
-
-    @RequestMapping(value = "/savecareer")
-    @ResponseBody
-    public String savecareer(MultipartHttpServletRequest request) throws Exception {
-
-        CareerinfoDto careerDto = new CareerinfoDto();
-
-        String cname = request.getParameter("cname");
-        String cment = request.getParameter("cment");
-        String sdate = request.getParameter("sdate");
-        String edate = request.getParameter("edate");
-
-        careerDto.setCicarname(cname);
-        careerDto.setCicomment(cment);
-        careerDto.setStartdate(sdate);
-        careerDto.setEnddate(edate);
-        careerinfoService.saveCareer(careerDto);
-        return "ok";
-    }
-
-
-
     @RequestMapping(value = "/deletecareer")
-    public String deletecareer(@RequestParam(value = "cno",required = false) Integer cno) throws Exception{
+    public String deletecareer(@RequestParam(value = "cno",required = false) Long cno) throws Exception{
         return "";
     }
 
--- src/main/java/com/hoondragonite/aboutme/domain/careerinfo/Careerinfo.java
+++ src/main/java/com/hoondragonite/aboutme/domain/careerinfo/Careerinfo.java
@@ -3,23 +3,28 @@ package com.hoondragonite.aboutme.domain.careerinfo;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 
 @Getter
+@Setter
 @Entity
 @NoArgsConstructor
 @EntityListeners(AuditingEntityListener.class)
 public class Careerinfo {
     @Id
+    //@GeneratedValue(generator="system-uuid")
+   // @GenericGenerator(name="system-uuid", strategy = "uuid")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int cno;
+    private Long cno;
 
-    @Column(length=200, nullable = true)
+    @Column(length=255, nullable = true)
     private String cicarname;
 
-    @Column(columnDefinition = "TEXT", nullable = true)
+    @Column(length=255, nullable = true)
     private String cicomment;
 
     @Column(length = 8,nullable = true)
@@ -29,7 +34,7 @@ public class Careerinfo {
     private String enddate;
 
     @Builder
-    public Careerinfo(int cno, String cicarname, String cicomment, String startdate, String enddate){
+    public Careerinfo(Long cno, String cicarname, String cicomment, String startdate, String enddate){
         this.cno = cno;
         this.cicarname = cicarname;
         this.cicomment = cicomment;
