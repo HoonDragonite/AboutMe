7ac64402d27afd46f3bde8b804dc3fc832ae4480
Bae <qpyu66@gmail.com>
2020-11-28 16:08:34 +0900

    수정ok

--- src/main/resources/static/js/careerinfo.js
+++ src/main/resources/static/js/careerinfo.js
@@ -17,7 +17,7 @@ function careerloadEvents() {
         $(".cdel").on('click', function () {
             deletecareer();
         });
-        $("#savecareer").on('click', function () {
+        $(document).on("click","#savecareer",function(){
             savecareer();
         });
     });
@@ -32,12 +32,10 @@ function getCareerList() {
         type: "POST",
         success: function (obj) {
             getCareerListCallback(obj);
-            console.log("getcareerlist > ", obj);
-            //	alert("board list json ok");
+            //console.log("getcareerlist > ", obj);
         },
         error: function (xhr, status, error) {
         }
-
     });
 }
 
@@ -61,15 +59,12 @@ function getCareerListCallback(obj) {
 
             str = "";
             str += "<tr>";
-            // str += "<td>" + "<a href=careermodify?cno=" + cno + ">" + cname
-            //     + "</a>" + "</td>";
             str += "<td id='cmcname" + cno + "'>" + cname + "</td>";
             str += "<td id='cmcment" + cno + "'>" + cment + "</td>";
             str += "<td id='cmsdate" + cno + "'>" + sdate + "</td>";
             str += "<td id='cmedate" + cno + "'>" + edate + "</td>";
             str += "<td>" + "<button id='cmodi' onclick='modifycareer(" + cno + ")'>수정</button>";
             str += "<button id='cdel' onclick='deletecareer(" + cno + ")'>삭제</button>" + "</td>";
-
             str += "</tr>";
 
             $("#citbody").append(str);
@@ -83,7 +78,6 @@ function getCareerListCallback(obj) {
     }
 }
 
-
 function addcareer() {
     $("#cinfotable").append(
         "<tr>"
@@ -91,9 +85,7 @@ function addcareer() {
         + "<td><input type='text' id='cment'></td>"
         + "<td><input type='text' id='sdate' placeholder='20201111'></td>"
         + "<td><input type='text' id='edate' placeholder='20201111'></td>"
-        // +"<td><button class='cmodify' value='수정'>수정</button>"
-        // +"<button class='cdel' value='삭제'>삭제</button>"
-        // +"</td>"
+        + "<td><button id='savecareer' value='저장'>저장 </button></td>"
         + "</tr>"
     );
 }//addcareer
@@ -123,19 +115,16 @@ function savecareer() {
             success: function (data) {
                 console.log(data);
                 alert("저장되었습니다.");
-                location.href = "/cinfo";
+                //location.href = "/cinfo";
             }, error: function (request, status, error) { //+request.responseText
                 location.href = "/cinfo";
-                //alert("code:"+request.status+"\n"+"message:"+"\n"+"error:"+error);
+                //alert("code:"+request+"\n"+"message:"+"\n"+"error:"+error);
             }
         });
     }
 }//savecareer
 
 function deletecareer(obj) {
-    // var params = {
-    //     cno : $(".cno").val()
-    // };
     console.log("deletecareer> ", obj);
 
     var yn = confirm("경력사항을 삭제하시겠습니까?");
@@ -149,7 +138,8 @@ function deletecareer(obj) {
             success: function (obj) {
                 console.log("delete obj>> " + obj);
                 alert("삭제되었습니다");
-                location.href = "/cinfo";
+                getCareerList();
+               // location.href = "/cinfo";
             }, error: function (request, status, error) {
                 alert("code:" + request.status + "\n" + "message:" + request.responseText + "\n" + "error:" + error);
             }
@@ -172,7 +162,6 @@ function modifycareer(mobj) {
         },
         error: function (xhr, status, error) {
         }
-
     });
 }
 
@@ -183,20 +172,17 @@ function cmCallback(mobj, obj) {
     var list = obj;
     var listLen = obj.length;
     var mo = mobj-1;
-    console.log("---------");
-    console.log("cmcallback mobj >", mobj, ", obj>", obj);
-    console.log("list[mobj].cno > ",list[mo].cno);
+    //console.log("list[mobj].cno > ",list[mo].cno);
 
     $("#citbody").empty();
-    //getCareerListCallback(obj);
 
     str += "<tr>"
     str += "<td><input type='text' id='cmcname'></td>"
     str += "<td><input type='text' id='cmcment'></td>"
     str += "<td><input type='text' id='cmsdate' ></td>"
     str += "<td><input type='text' id='cmedate' ></td>"
-    str += "<td>" + "<button id='modicareer' onclick='modicareer("+mo+")'>확인</button>";
-    // str += "<button id='cdel' onclick='deletecareer(" + cno + ")'>삭제</button>" + "</td>";
+    str += "<td>" + "<button id='modicareer' onclick='modicareer("+mobj+")'>확인</button>";
+     str += "<button id='cback' onclick='getCareerList()'>취소</button>" + "</td>";
     str += "</tr>"
     $("#citbody").append(str);
 
@@ -205,67 +191,11 @@ function cmCallback(mobj, obj) {
         $("#cmsdate").val(list[mo].startdate);
         $("#cmedate").val(list[mo].enddate);
 
-
-/*
-    for (var a = 0; a < listLen; a++) {
-        var cno = list[a].cno;
-        var cname = list[a].cicarname;
-        var cment = list[a].cicomment;
-        var sdate = list[a].startdate;
-        var edate = list[a].enddate;
-        console.log("for a > ", cno);
-        var sa = (cno == mobj);
-
-        str += "<tr>";
-        str += "<td id='cmcname" + cno + "'>" + cname + "</td>";
-        str += "<td id='cmcment" + cno + "'>" + cment + "</td>";
-        str += "<td id='cmsdate" + cno + "'>" + sdate + "</td>";
-        str += "<td id='cmedate" + cno + "'>" + edate + "</td>";
-        str += "<td>" + "<button id='cmodi' onclick='modifycareer(" + cno + ")'>수정</button>";
-        str += "<button id='cdel' onclick='deletecareer(" + cno + ")'>삭제</button>" + "</td>";
-        str += "</tr>";
-
-        // if (cno == mobj) {
-        //     str += "<tr>"
-        //     str += "<td><input type='text' id='cmcname'+cno></td>"
-        //     str += "<td><input type='text' id='cmcment'+cno></td>"
-        //     str += "<td><input type='text' id='cmsdate'+cno></td>"
-        //     str += "<td><input type='text' id='cmedate'+cno></td>"
-        //     str += "<td>" + "<button id='savecareer' onclick='savecareer()'>확인</button>";
-        //     str += "<button id='cdel' onclick='deletecareer(" + cno + ")'>삭제</button>" + "</td>";
-        //     str += "</tr>"
-        //     // $("#cmcname"+mobj).val(cname);
-        //     // $("#cmcment"+mobj).val(cment);
-        //     // $("#cmsdate"+mobj).val(sdate);
-        //     // $("#cmedate"+mobj).val(edate);
-        //
-        //     //$("#citbody").append(str);
-        //     continue;
-        // }
-        // else {
-        //
-        // }//else
-        $("#citbody").append(str);
-        if (cno == mobj) {
-
-            $("#cmcname" + mobj).val(cname);
-            $("#cmcment" + mobj).val(cment);
-            $("#cmsdate" + mobj).val(sdate);
-            $("#cmedate" + mobj).val(edate);
-        }
-
-    }//for
-
-*/
-
 }//cmCallback
 
 
-
-
-
-function modicareer(mo) {
-    var cno = mo+1;
+function modicareer(mobj) {
+    var cno = mobj;
     var cname = $('#cmcname').val();
     var cment = $('#cmcment').val();
     var sdate = $('#cmsdate').val();
@@ -280,7 +210,6 @@ function modicareer(mo) {
     };
     console.log("objParm=>>" + JSON.stringify(objParam));
 
-
     $.ajax({
         url: "/modifycareer/"+cno,
         dataType: "JSON",
@@ -288,16 +217,16 @@ function modicareer(mo) {
         data: JSON.stringify(objParam),
         contentType: "application/json",
         success: function (obj) {
-            //cmCallback(mobj, obj);
-            //getCareerListCallback(obj);
+            getCareerList();
             console.log("modifycareer obj> ", obj);
-            location.href = "/cinfo";
+            //location.href = "/cinfo";
         },
         error: function (xhr, status, error) {
+            getCareerList();
             location.href = "/cinfo";
-            // alert(error);
+            //alert(error);
         }
-
     });
 
 }
+
