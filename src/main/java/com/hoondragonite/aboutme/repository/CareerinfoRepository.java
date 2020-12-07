fb963749b178696096430f32eb3e3a3c5e80bcb1
HoonDragonite <lsoonnoon@gmail.com>
2020-12-07 19:56:49 +0900

    delete

--- src/main/java/com/hoondragonite/aboutme/repository/CareerinfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hoondragonite.aboutme.repository;
-
-import com.hoondragonite.aboutme.domain.careerinfo.Careerinfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CareerinfoRepository extends JpaRepository<Careerinfo, Long> {
-
-}
