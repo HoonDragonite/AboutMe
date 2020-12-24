3a8fca236b664e4a020126a0e2bbdcbe9458b4c3
HoonDragonite <lsoonnoon@gmail.com>
2020-12-24 13:48:23 +0900

    template domain create

---
+++ src/main/java/com/hoondragonite/aboutme/domain/template/template.java
@@ -0,0 +1,22 @@
+package com.hoondragonite.aboutme.domain.template;
+
+import com.hoondragonite.aboutme.domain.BaseTimeEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class template extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long templateSeq;
+    @Column(length = 50)
+    private String templateName;
+    @Column(length = 200)
+    private String templateDesc;
+    @Column(length = 100)
+    private String templateImage;
+}
